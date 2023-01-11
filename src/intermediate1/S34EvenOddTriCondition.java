7832cc9dfa688c3263a04bf1beaf370d39ae4c3a
yularete <keinikeiwo@gmail.com>
2023-01-11 09:28:57 +0900

    035: 간단한 if 연산자는 삼항연산자를 이용한다.

---
+++ src/intermediate1/S34EvenOddTriCondition.java
@@ -0,0 +1,15 @@
+package intermediate1;
+
+public class S34EvenOddTriCondition {
+    public static void main(String[] args) {
+        int temp = 99;
+        temp = (temp % 2 == 1) ? temp * 3 + 1 : temp / 2;
+        ß
+        System.out.printf("계산 후=%d\n", temp);
+    }
+}
+
+// if(a) { d=b
+// }else{d=c}는
+// d=a?b:c와 같다.
+// b, c, d의 타입이 동일해야 하고, 연산 결과는 d에 대입한다/
