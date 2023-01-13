a448eb8ecfff4aeaa7668a7f1fba766bc43135f6
yularete <keinikeiwo@gmail.com>
2023-01-13 23:54:45 +0900

    046:  while을 이용해 서로 다른 세 정수 만들기

---
+++ src/intermediate1/S46BioRandom.java
@@ -0,0 +1,16 @@
+package intermediate1;
+
+public class S46BioRandom {
+    public static void main(String[] args) {
+        int range=10;
+        int start=1;
+        int a=0, b=0, c=0;
+        while (true){
+            a=(int)(range*Math.random()+start);
+            b=(int)(range*Math.random()+start);
+            c=(int)(range*Math.random()+start);
+            if(a!=b && b!=c && c!=a ) break;
+        }
+        System.out.printf("%d\t%d\t%d",a,b,c);
+    }
+}
